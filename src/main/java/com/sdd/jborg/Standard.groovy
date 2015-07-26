720584eaf6787511c8738a5b0f172428432c2250
Mike Smullin <mike@smullindesign.com>
2015-07-25 19:50:03 -0600

    another way

--- src/main/java/com/sdd/jborg/Standard.groovy
+++ src/main/java/com/sdd/jborg/Standard.groovy
@@ -21,10 +21,9 @@ public class Standard
     public static final int SERIAL = 1;
     public static final int PARALLEL = 2;
 
-    public static Async then(final int type)
+    public static void then(final Callback0 cb)
     {
         queue.add(cb);
-        return Async.instance;
     }
 
     public static void go()
@@ -39,105 +38,100 @@ public class Standard
      * methods that may only be called asynchronously
      * e.g., from within second loop
      */
-    public static final class Async
+    public static void die(final String reason)
     {
-        private static final Async instance = new Async();
+        Logger.stderr "Aborting. Reason: ${reason}"
+        System.exit 1
+    }
 
-        public static void die(final String reason)
+    public static String decrypt(final String s)
+    {
+        return "";
+    }
+
+    public static Callback0 chown(final Map o = [:], final String path)
+    {
+        if (!o['owner'] || !o['group'])
+            die "chown owner and group are required."
+        return {
+            execute "chown ${o['owner']}.${o['group']} ${path}"
+        }
+    }
+
+    public static Callback0 chmod(final Map o = [:], final String path)
+    {
+        return { execute "chmod ${o['mode']} ${path}" }
+    }
+
+    public static abstract class DirectoryParams
+    {
+        String owner;
+        String group;
+        String mode;
+        String sudo;
+
+        public DirectoryParams setOwner(final String owner)
         {
-            Logger.stderr "Aborting. Reason: ${reason}"
-            System.exit 1
+            this.owner = owner
+            return this
         }
 
-        public static String decrypt(final String s)
+        public DirectoryParams setGroup(final String group)
         {
-            return "";
+            this.group = group
+            return this
         }
 
-        public static Callback0 chown(final Map o = [:], final String path)
+        public DirectoryParams setMode(final String mode)
         {
-            if (!o['owner'] || !o['group'])
-                die "chown owner and group are required."
-            return {
-                execute "chown ${o['owner']}.${o['group']} ${path}"
+            this.mode = mode
+            return this
+        }
+
+        public DirectoryParams setSudo(final String sudo)
+        {
+            this.sudo = sudo
+            return this
+        }
+    }
+
+    public static DirectoryParams directory(final String path)
+    {
+        o['mode'] ?: '0755'
+        return {
+            execute "test -d ${path}", test: { code ->
+                if (code == 0)
+                    log 'Skipping existing directory.'
+                else
+                    execute
             }
+            execute "mkdir -p ${path}"
         }
+    }
 
-        public static Callback0 chmod(final Map o = [:], final String path)
-        {
-            return { execute "chmod ${o['mode']} ${path}" }
-        }
+    public static void execute(final Map o = [:], final String cmd)
+    {
+        ssh.cmd cmd
+    }
 
-        public static abstract class DirectoryParams
-        {
-            String owner;
-            String group;
-            String mode;
-            String sudo;
+    public static Callback0 template(final String path)
+    {
+        return {
 
-            public DirectoryParams setOwner(final String owner)
-            {
-                this.owner = owner
-                return this
-            }
+        };
+    }
 
-            public DirectoryParams setGroup(final String group)
-            {
-                this.group = group
-                return this
-            }
+    public static Callback0 upload(final String path)
+    {
+        return {
 
-            public DirectoryParams setMode(final String mode)
-            {
-                this.mode = mode
-                return this
-            }
+        };
+    }
 
-            public DirectoryParams setSudo(final String sudo)
-            {
-                this.sudo = sudo
-                return this
-            }
-        }
+    public static Callback0 remoteFileExists(final String path)
+    {
+        return {
 
-        public static DirectoryParams directory(final String path)
-        {
-            o['mode'] ?: '0755'
-            return {
-                execute "test -d ${path}", test: { code ->
-                    if (code == 0)
-                        log 'Skipping existing directory.'
-                    else
-                        execute
-                }
-                execute "mkdir -p ${path}"
-            }
-        }
-
-        public static void execute(final Map o = [:], final String cmd)
-        {
-            ssh.cmd cmd
-        }
-
-        public static Callback0 template(final String path)
-        {
-            return {
-
-            };
-        }
-
-        public static Callback0 upload(final String path)
-        {
-            return {
-
-            };
-        }
-
-        public static Callback0 remoteFileExists(final String path)
-        {
-            return {
-
-            };
-        }
+        };
     }
 }
